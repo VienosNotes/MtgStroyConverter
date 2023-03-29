beefdf61f9993c903b45f24586e45297da365002
D.Aoki <aoki@tac-netweb.co.jp>
2023-03-29 18:50:03 +0900

    Initial commit

---
+++ MtgStroyConverter/ArticleUtils.fs
@@ -0,0 +1,51 @@
+﻿module MtgStroyConverter.ArticleUtils
+open System
+open System.IO
+open System.Text
+
+type Chapter = { title: string; url: string }
+type Article = {
+    index: int 
+    chapter: string 
+    subtitle: string
+    body: string
+    url: string
+}
+
+
+let printChapter chapter =
+    Console.WriteLine($"{{ title: {chapter.title}, url: {chapter.url} }}")
+    
+let currentTime = "2023/03/07/ 12:00:00"
+
+let createArticleYaml (article: Article): string  =
+   let builder = StringBuilder()
+   builder.Append("---\r\n")
+       .Append($"index: \"{article.index}\"\r\n")
+       .Append($"href: \"{article.url}\"\r\n")
+       .Append($"chapter: \"{article.chapter}\"\r\n")
+       .Append($"subchapter: \"\"\r\n")
+       .Append($"subtitle: {article.subtitle}\r\n")
+       .Append($"file_subtitle: {article.subtitle}\r\n")
+       .Append($"subdate: \"{currentTime}\"\r\n")
+       .Append($"subupdate: \"{currentTime}\"\r\n")
+       .Append($"element:\r\n")
+       .Append($"\tdata_type: html\r\n")
+       .Append($"\tintroduction: \"\"\r\n")
+       .Append($"\tpostscript: \"\"\r\n")
+       .Append($"\tbody: |-\r\n")
+   |> ignore       
+   for line in article.body.Split("\r\n") do
+     builder.Append($"\t{line}\r\n") |> ignore
+   builder.ToString()
+         
+let saveArticle (article: Article): unit =
+    let baseDir = "output/本文"
+    let name = $"{baseDir}/{article.index} {article.subtitle}.yml"
+    if File.Exists(name) then File.Delete(name)
+    if not (Directory.Exists(baseDir)) then Directory.CreateDirectory(baseDir) |> ignore
+    let file = new StreamWriter(name)
+    file.Write(createArticleYaml article)
+    file.Dispose()
+    ()
+    
---
+++ MtgStroyConverter/Program.fs
@@ -0,0 +1,53 @@
+﻿open FSharp.Data
+open MtgStroyConverter.ArticleUtils
+
+let parseBody (articleUrl: string) : string = $"this is dummy no text ({articleUrl})"
+
+let extractArticle  (index: int) (chapter: string) (articleNode: HtmlNode): Article =
+    let href = (articleNode.Descendants ["a"] |> Seq.head).AttributeValue("href")
+    let subtitle = (articleNode.Descendants ["p"] |> Seq.item 1).InnerText()
+    let body = parseBody href
+    { index = index; subtitle = subtitle; chapter = chapter; body = body; url = href }
+
+let parseChapterIndex  (startIndex: int) (chapter:string) (doc: HtmlDocument) : Article seq =
+    let articlesSection = doc.Descendants ["section"]
+                          |> Seq.filter (fun n -> n.HasClass("list-article"))
+                          |> Seq.head
+    let articlesList = articlesSection.Descendants ["ul"] |> Seq.head
+    let articles = articlesList.Descendants ["li"] |> Seq.rev
+    // TODO: remove "take 1"
+    articles |> Seq.take 1 |> Seq.map (extractArticle startIndex chapter)
+
+let createArticleIndex (url: string) (chapter: string): Article seq =
+    let articleIndexHtml = HtmlDocument.Load(url)
+    parseChapterIndex 0 chapter articleIndexHtml 
+
+/// Create a Chapter object from a [li] HtmlNode in the index page.
+let extractListItem (node: HtmlNode) : Chapter =
+    let titleNode = node.Descendants ["h3"] |> Seq.head
+    let title = titleNode.InnerText()
+    let urlNode = node.Descendants [ "a" ] |> Seq.head
+    let url = urlNode.AttributeValue("href")
+    { title = title; url = url }
+
+/// Return sequence of Chapters with parsing HtmlDocument.
+let parseIndex (doc: HtmlDocument) : Chapter seq =
+    let articlesUl = doc.Descendants ["ul"]
+                     |> Seq.filter (fun ul -> ul.HasClass("backnumber-list-img"))
+                     |> Seq.head
+    let parts = articlesUl.Descendants ["li"] |> Seq.rev
+    parts |> Seq.map extractListItem
+
+/// Return sequence of Chapters from the url of index page.
+let createIndex (url: string): Chapter seq =
+    let indexHtml = HtmlDocument.Load(url)
+    parseIndex indexHtml    
+
+[<EntryPoint>]
+let main _ =
+    let index = (createIndex "https://mtg-jp.com/reading/ur/") |> List.ofSeq
+    let test = index |> List.item 0
+    let articles = createArticleIndex test.url test.title
+    for art in articles do
+        saveArticle art
+    0
